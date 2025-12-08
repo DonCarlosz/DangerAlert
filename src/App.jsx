b1768305ed5783e3e3c378d38a5528dbde9dece3
DonCarlosz <akhuemokhanisaiah@gmail.com>
2025-12-09 00:01:23 +0100

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SplashPage from './components/SplashPage'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SplashPage/>}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
   )
 }
