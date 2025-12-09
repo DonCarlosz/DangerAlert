18ef7b7ea0ca637b588cc51724639432476dc20c
DonCarlosz <akhuemokhanisaiah@gmail.com>
2025-12-09 23:36:17 +0100

    h

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import SplashPage from './components/SplashPage'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
+import NotFound from './components/Notfound'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<SplashPage/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
   )
 }
